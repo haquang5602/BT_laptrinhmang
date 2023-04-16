6bb9ecc4c28ae3e043fa5ee442f068d73321be27
haquang5602 <haquang5602@gmail.com>
2023-04-16 23:51:40 +0700

    update_4

--- btvn_tuan4/tcp_server.cpp
+++ btvn_tuan4/tcp_server.cpp
@@ -5,86 +5,73 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define MAX_BUFFER_SIZE 2048
+#define MAX_BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
+    int server_sock = 0, client_sock = 0;
+    struct sockaddr_in server_address, client_address;
+    char buffer[MAX_BUFFER_SIZE] = {0};
+
     if (argc != 4) {
-        printf("Usage: tcp_server <server_port> <welcome_message_file> <client_message_file>\n");
-        exit(1);
+        printf("Usage: %s <port> <hello string> <file name>\n", argv[0]);
+        return -1;
     }
 
-    // Khởi tạo một socket
-    int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server_socket == -1) {
-        perror("Failed to create socket");
-        exit(1);
+    // create a socket
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error\n");
+        return -1;
     }
 
-// Thiết lập địa chỉ máy chủ và cổng
-    struct sockaddr_in server_address;
-    // memset(&server_address, 0, sizeof(server_address));
+    // set server address and port
+    memset(&server_address, '0', sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     server_address.sin_port = htons(atoi(argv[1]));
 
-    // Liên kết ổ cắm với địa chỉ máy chủ và cổng
-    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
-        perror("Failed to bind socket");
-        close(server_socket);
-        exit(1);
+    // bind socket to server address and port
+    if (bind(server_sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        printf("Bind failed\n");
+        return -1;
     }
 
-    // Listen for incoming connections
-    if (listen(server_socket, 5) == -1) {
-        perror("Failed to listen for connections");
-        close(server_socket);
-        exit(1);
+    // listen for incoming connections
+    if (listen(server_sock, 3) < 0) {
+        printf("Listen failed\n");
+        return -1;
     }
 
-    // Load the welcome message from file
-    char welcome_message[MAX_BUFFER_SIZE];
-    FILE *welcome_file = fopen(argv[2], "r");
-    if (welcome_file == NULL) {
-        perror("Failed to open welcome message file");
-        close(server_socket);
-        exit(1);
+    // accept incoming connections
+    socklen_t client_address_len = sizeof(client_address);
+    if ((client_sock = accept(server_sock, (struct sockaddr *)&client_address, &client_address_len)) < 0) {
+        printf("Accept failed\n");
+        return -1;
     }
-    fgets(welcome_message, MAX_BUFFER_SIZE, welcome_file);
-    fclose(welcome_file);
 
-
-    char buffer[MAX_BUFFER_SIZE];
-    // Chấp nhận các kết nối đến và xử lý chúng
-    struct sockaddr_in clientAddr;
-    int clientAddrLen = sizeof(server_address);
-
-    int client = accept(server_socket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-    while (1) {
-        FILE *out_file = fopen(argv[3], "a");
-
-        // Nhận tin nhắn từ khách hàng và ghi chúng vào một tệp
-        int bytes_received = recv(client, buffer, MAX_BUFFER_SIZE, 0);
-
-        if (bytes_received == -1) {
-            perror("Failed to receive message from client");
-            break;
-        } else if (bytes_received == 0) {
-            printf("CLient disconnected!\n");
-            break;
-        } else {
-            printf("%s\n", welcome_message);
-            if(sizeof(buffer) > MAX_BUFFER_SIZE) {
-                printf("MAX\n");
-            }
-            printf("Message from client: %s\n", buffer);
-            fprintf(out_file, "%s\n", buffer);
-            // *buffer = "";
-        }
-
-        fclose(out_file);
+    // send hello string to client
+    if (send(client_sock, argv[2], strlen(argv[2]), 0) < 0) {
+        printf("Message send failed\n");
+        return -1;
     }
-    // Đóng socket
-    close(server_socket);
+
+    // open file for writing
+    FILE *file = fopen(argv[3], "w");
+    if (file == NULL) {
+        printf("File open failed\n");
+        return -1;
+    }
+
+    // receive data from client and save to file
+    int num_bytes_received = 0;
+    while ((num_bytes_received = recv(client_sock, buffer, MAX_BUFFER_SIZE, 0)) > 0) {
+        fwrite(buffer, 1, num_bytes_received, file);
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    // close file and sockets
+    fclose(file);
+    close(client_sock);
+    close(server_sock);
 
     return 0;
 }
